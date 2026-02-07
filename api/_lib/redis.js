81dab7557509c9c120f008d24a002dff4334a32f
Corey Gagnon <gagnon.corey@gmail.com>
2026-02-06 21:47:31 -0500

    setting up api

--- api/_lib/redis.js
+++ api/_lib/redis.js
@@ -1,8 +1,8 @@
 import { Redis } from '@upstash/redis';
 
 // Redis client configured from environment variables
-// UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN are set by Vercel
+// Vercel KV sets KV_REST_API_URL and KV_REST_API_TOKEN
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
